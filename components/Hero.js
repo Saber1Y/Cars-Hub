95d96a25d15443402c92bbcd4ecaad9debe8c2d2
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-19 22:26:55 +0100

    UPDATE: NAVBAR ADDED

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='flex-1 pt-36 px-2'>
-                <h1 className='hero__title'>
+                <h1 className='hero__title text-[#2b59ff]'>
                     Find, book, rent a car quick and super easy!
                 </h1>
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { CustomButton } from '..'
 
 const Navbar = () => {
   return (
-    <div>
-      NavBar
-    </div>
+    <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center px-6 py-4'>
+        <Link href="" className='flex justify-center items-center'>
+          <img src="./logo.svg" alt="car logo" width={117} height={18} className='object-logo' />
+        </Link>
+
+        <CustomButton
+         title="Sign in"
+         type="button"
+         containerStyles='text-primary-blue rounded-full bg-white min-w-[120px]'
+        />
+      </nav>
+    </header>
   )
 }
 
