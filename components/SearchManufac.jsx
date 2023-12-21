19b38fae40586be08cc329d9f5b734a0b505b4f4
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-21 20:36:06 +0100

    CREATE: API ENDPOINT

--- components/SearchManufac.jsx
+++ components/SearchManufac.jsx
@@ -17,7 +17,7 @@ const SearchManufac = ({ manufacturer, setManufacturer }) => {
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relativeeee w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image
@@ -42,13 +42,33 @@ const SearchManufac = ({ manufacturer, setManufacturer }) => {
             afterLeave={() => setQuery('')}
           >
             <Combobox.Options>
-              {FilteredManu.length === 0 && query !== "" && (
-                <Combobox.Option 
-                value={query}
-                className="search-manufacturer__options"
-                >
-                  Create "{query}"
-                </Combobox.Option>
+               {(FilteredManu.map((manu) => (
+                  <Combobox.Option 
+                  key={manu} 
+                  className={({ active }) => `relative search-menufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                  value={manu}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                      <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {manu}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
               )}
             </Combobox.Options>
           </Transition>
---
+++ utils/index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://car-api2.p.rapidapi.com/api/vin/1GTG6CEN0L1139305',
+  headers: {
+    'X-RapidAPI-Key': 'f746eb062fmsh70adfb33544717fp1515bcjsn6d2e53d0f0ba',
+    'X-RapidAPI-Host': 'car-api2.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
