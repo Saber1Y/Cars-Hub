9da1781d07fe13d7c0c9e12a946fd0283827975f
ayodeji <ayodejiakintobi1@gmail.com>
2024-04-06 13:39:04 +0100

    UPDATE: MODAL

--- components/CardCard.jsx
+++ components/CardCard.jsx
@@ -57,7 +57,7 @@ const CardCard = ({ car }) => {
         </div>
 
         <div className="car-card__btn-container">
-            <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-17px font-bold" rightIcon="/right-arrow.svg" handleClick={() => SetTrue(true)}  />
+            <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-17px font-bold" rightIcon="/right-arrow.svg" handleClick={() => setOpen(true)}  />
         </div>
       </div>
 
