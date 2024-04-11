456cab0a582d3a9fff7a409bf4be7f1880cfda40
ayodeji <ayodejiakintobi1@gmail.com>
2024-04-11 17:20:02 +0100

    UPDATE

--- components/SearchButton.jsx
+++ components/SearchButton.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const SearchButton = ({ otherstyles }) => {
   return (
-    <button type='submit' className={`-ml-3 z-10 ${otherstyles}`}></button>
-  )
-}
+    <button type="submit" className={`-ml-3 z-10 ${otherstyles}`}>
+      <Image
+        src="magnifiying-glass.svg"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+};
 
-export default SearchButton
+export default SearchButton;
--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,22 +1,45 @@
-"use client"
-import React, { useState } from 'react'
-import { SearchManufac } from '..'
+"use client";
+import React, { useState } from "react";
+import { SearchManufac } from "..";
+import SearchButton from "./SearchButton";
+import Image from "next/image";
 
 const Searchbar = () => {
+  const [modal, setModal] = useState("");
+  const [manufacturer, setManufacturer] = useState();
 
-  const [manufacturer, setManufacturer] = useState()
-
-  const handleSearch = () => {
-    
-  }
+  const handleSearch = () => {};
 
   return (
-    <form className='searchbar' onSubmit={handleSearch}> 
-      <div className='searchbar__item'>
-        <SearchManufac manufacturer={manufacturer} setManufacturer={setManufacturer} />
-      </div> 
-    </form>
-  )
-}
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufac
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherstyles="sm:hidden " />
+      </div>
 
-export default Searchbar
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+
+        <input
+          type="text"
+          name="model"
+          value={modal}
+          onClick={(e) => setModal(e.target.value)}
+          placeholder="model"
+          className="searchbar__input"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Searchbar;
